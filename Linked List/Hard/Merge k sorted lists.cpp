5d9563de0c959ff79ff8ca7ad40fb28092109e5b
JatinBajaj-jb <jatinbajaj757@gmail.com>
2025-08-14 22:27:31 +0530

    Update Merge k sorted lists.cpp

--- Linked List/Hard/Merge k sorted lists.cpp
+++ Linked List/Hard/Merge k sorted lists.cpp
@@ -35,3 +35,48 @@ public:
         
     }
 };
+// Approach 2 : I seperated two list then used a merger feature on them , 
+class Solution {
+private:
+    ListNode* mergetwo(ListNode* l1,ListNode* l2){
+        if(!l1){
+            return l2;
+        }
+        if(!l2){
+            return l1;
+        }
+        if(l1->val>l2->val){
+            l2->next=mergetwo(l1,l2->next);
+            return l2;
+        }
+        else{
+            l1->next=mergetwo(l1->next,l2);
+            return l1;
+        }
+    }
+
+    ListNode* merger(int st,int end,vector<ListNode*>& lists){
+        if(st==end){
+            return lists[st];
+        }
+        if(st>end){
+            return NULL;
+        }
+        int mid=st+(end-st)/2;
+        // if(mid-st+1==2){
+        //     return mergetwo(lists[st],lists[st+1]);
+        // }
+        ListNode* l1=merger(st,mid,lists);
+        ListNode* l2=merger(mid+1,end,lists);
+        ListNode* ans=mergetwo(l1,l2);
+        return ans;
+        }
+    
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int st=0;
+        int end=lists.size()-1;
+        ListNode* result=merger(st,end,lists);
+        return result;
+    }
+};
