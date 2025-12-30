b35ea4a485a1db12dd868000f3f7d0119b3d76d2
JatinBajaj-jb <jatinbajaj757@gmail.com>
2025-12-30 15:55:05 +0530

    Update Merge two sorted Lists.cpp

--- Linked List/Easy/Merge two sorted Lists.cpp
+++ Linked List/Easy/Merge two sorted Lists.cpp
@@ -1,3 +1,4 @@
+// I used a recursive approach to the question
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
@@ -15,5 +16,27 @@ public:
             list2->next=mergeTwoLists(list1,list2->next);
             return list2;
         }
+    }
+};
+// Approach 2 : I used iterative approach to solve the question
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0);
+        ListNode* result=&dummy;
+        if(!list1 && !list2) return nullptr;
+        while(list1 && list2){
+            if(list1->val <=list2->val){
+                result->next=list1;
+                list1=list1->next;
+            }
+            else{
+                result->next=list2;
+                list2=list2->next;
+            }
+            result=result->next;
+        }
+        result->next=list1?list1:list2;
+        return dummy.next; 
     }
 };
