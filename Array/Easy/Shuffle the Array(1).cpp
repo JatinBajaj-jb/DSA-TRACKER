836f23725ab3c0eb876c5259853d3cdf9bf6f4bb
JatinBajaj-jb <jatinbajaj757@gmail.com>
2025-12-31 14:17:49 +0530

    Update Shuffle the Array(1).cpp

--- Array/Easy/Shuffle the Array(1).cpp
+++ Array/Easy/Shuffle the Array(1).cpp
@@ -11,3 +11,22 @@ public:
         return result;
     }
 };
+// Attempt 2 : solved this question with almost the same method
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        int mid=n-1;
+        vector<int> result(2*n,0);
+        int indx1=0;
+        int indx2=n;
+        for(int i=0;i<2*n;i++){
+            if(i%2==0){
+                result[i]=nums[indx1++];
+            }
+            else{
+                result[i]=nums[indx2++];
+            }
+        }
+        return result;
+    }
+};
