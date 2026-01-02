56f7352d3e1a0321d544f5c174c7fe3d4290c53a
JatinBajaj-jb <jatinbajaj757@gmail.com>
2026-01-02 10:26:57 +0530

    Update Word Pattern(1).cpp

--- Strings/Easy/Word Pattern(1).cpp
+++ Strings/Easy/Word Pattern(1).cpp
@@ -25,3 +25,32 @@ public:
         return indx==pattern.length();   
     }
 };
+
+// Attempt 2: I solved the question by using the same method 
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<string,char> mp1;
+        unordered_map<char,string> mp2;
+        int indx=0;
+        int count=0;
+        string temp="";
+        stringstream ss(s); 
+        while(getline(ss,temp,' ')){
+            count++;
+            if(count>pattern.size()) return false;
+            if(mp1.find(temp)==mp1.end()){
+                if(mp2.find(pattern[indx])==mp2.end()){
+                    mp1[temp]=pattern[indx];
+                    mp2[pattern[indx]]=temp;
+                }
+                else return false;
+            }
+            else{
+                if(mp1[temp]!=pattern[indx] || mp2[pattern[indx]]!=temp)return false;
+            }
+            indx++;
+        }
+        return indx==pattern.length();
+    }
+};
