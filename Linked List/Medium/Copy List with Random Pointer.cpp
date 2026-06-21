44aaf59bcab5013d8af870729f0dabc7d6a35ee3
JatinBajaj-jb <jatinbajaj757@gmail.com>
2026-06-21 12:28:59 +0530

    Create Copy List with Random Pointer.cpp

---
+++ Linked List/Medium/Copy List with Random Pointer.cpp
@@ -0,0 +1,20 @@
+// Approach 1 : I used unordered map to store the copy Nodes 
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head) return nullptr;
+        Node* curr=head;
+        unordered_map<Node*,Node*> mp;
+        while(curr){
+           mp[curr]=new Node(curr->val);
+           curr=curr->next;
+        }
+        curr=head;
+        while(curr){
+            mp[curr]->next=mp[curr->next];
+            mp[curr]->random=mp[curr->random];
+            curr=curr->next;
+        }
+        return mp[head];
+    }
+};
